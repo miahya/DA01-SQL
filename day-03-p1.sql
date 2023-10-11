0878c48c7ab23bd9286207494b906d5a032eac26
miahya <147453453+miahya@users.noreply.github.com>
2023-10-12 00:57:42 +0100

    Update day-03-p1.sql

--- day-03-p1.sql
+++ day-03-p1.sql
@@ -1,16 +1,87 @@
-/Exercise 1
+-- Exercise 1
 select name
 from city
 where population > 120000
 and (countrycode = 'USA');
 
-/Exercise 2
+-- Exercise 2
 select *
 from city
 where countrycode = 'JPN';
 
-/Exercise 3
+-- Exercise 3
 select city, state
 from station;
 
-/Exercise 4
+-- Exercise 4
+select distinct city
+from station
+where (city like 'A%') 
+or (city like 'E%') 
+or (city like 'I%') 
+or (city like 'O%') 
+or (city like 'U%');
+
+-- Exercise 5
+select distinct city
+from station
+where (city like '%a') 
+or (city like '%e') 
+or (city like '%i') 
+or (city like '%o') 
+or (city like '%u');
+
+-- Exercise 6 (tsao la wrong answer*)
+select distinct city
+from station
+where not (city like 'A%') 
+or (city like 'E%') 
+or (city like 'I%') 
+or (city like 'O%') 
+or (city like 'U%');
+
+-- Exercise 7
+select name
+from employee
+order by name ASC;
+
+-- Exercise 8
+select name
+from employee
+where salary >= 2000 and months < 10
+order by employee_id;
+
+-- Exercise 9
+elect product_id
+from products
+where low_fats = 'Y' and recyclable = 'Y';
+
+-- Exercise 10 (tsao phai them condition is null*)
+select name
+from customer
+where referee_id != 2 or referee_id is null;
+
+-- Exercise 11
+select name, population, area
+from world
+where area >= 3000000 or population >= 25000000;
+
+-- Exercise 12 (((())))
+select distinct author_id as id
+from Views
+where author_id = viewer_id
+order by author_id;
+
+-- Exercise 13
+SELECT part, assembly_step
+FROM parts_assembly
+WHERE finish_date is NULL;
+
+-- Exercise 14
+select * from lyft_drivers
+where (yearly_salary <= 30000) or (yearly_salary >= 70000);
+
+-- Exercise 15
+select advertising_channel 
+from uber_advertising
+where money_spent > 100000 and year = 2019;
