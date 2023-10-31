1b78c7e427c2db099730ba391f3655961abcb66d
miahya <147453453+miahya@users.noreply.github.com>
2023-10-31 01:04:32 +0000

    Update day-11.sql

--- day-11.sql
+++ day-11.sql
@@ -9,8 +9,13 @@ GROUP BY COUNTRY.Continent;
 
 --- Exercise 3
 
---- Exercise 4
-
+--- Exercise 4 (vì category chỉ có đúng 3 loại đó nên không cần câu lệnh WHERE để specify, nếu nhiều hơn thì mới cần)
+SELECT customer_id
+FROM products
+JOIN customer_contracts ON customer_contracts.product_id = products.product_id
+## --- WHERE product_category IN ('Analytics', 'Containers', 'Compute')
+GROUP BY customer_id
+HAVING COUNT(DISTINCT product_category) = 3;
 
 --- Exercise 5
 
