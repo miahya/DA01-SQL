b62a40c6bee2c1576b3c848cb8f48fa2a0967882
miahya <147453453+miahya@users.noreply.github.com>
2023-11-06 17:12:34 +0000

    Update day-13.sql

--- day-13.sql
+++ day-13.sql
@@ -24,7 +24,13 @@ FROM spend_cte
 WHERE ranking <=2;
 
 --- exercise 3:
+SELECT COUNT(policy_holder_id) AS member_count
+FROM(SELECT policy_holder_id, COUNT(case_id)
+     FROM callers
+     GROUP BY policy_holder_id
+     HAVING COUNT(case_id) >= 3) as count_table;
 
+--- exercise 4:
 
 
 
