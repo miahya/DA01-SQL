1a1ff6b2444ee25aed5107e389e482d8c4fa44db
miahya <147453453+miahya@users.noreply.github.com>
2023-11-06 16:34:55 +0000

    Update day-13.sql

--- day-13.sql
+++ day-13.sql
@@ -9,8 +9,21 @@ COUNT(DISTINCT company_id) AS duplicate_companies
 FROM post_duplicate
 WHERE so_bai > 1;
 
---- exercise 2: phải sdung hàm RANK thay LIMIT vì là top2 ở mỗi category, limit order by là tính chung trên toàn data
+--- exercise 2: phải sdung hàm RANK thay LIMIT vì là top2 ở mỗi category, limit order by là tính chung trên toàn data => tạo 1 bảng mới có cột ranking r filter sau
+WITH spend_cte AS
+(SELECT category, product, 
+sum(spend) AS total_spend,
+RANK() OVER(PARTITION BY category 
+            ORDER BY SUM(spend) desc) AS ranking
+FROM product_spend
+WHERE EXTRACT(year FROM transaction_date) = '2022'
+GROUP BY category, product)
 
+SELECT category, product, total_spend
+FROM spend_cte
+WHERE ranking <=2;
+
+--- exercise 3:
 
 
 
