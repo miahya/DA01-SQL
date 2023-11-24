973ca81b222ea0c975ca29e0e86c2777871b4871
miahya <147453453+miahya@users.noreply.github.com>
2023-11-24 01:12:43 +0000

    Update day-16.sql

--- day-16.sql
+++ day-16.sql
@@ -106,6 +106,21 @@ from products
 where product_id not in (select product_id from cte_product)
 order by product_id
 
-
+*****
+(select product_id, new_price as price
+from products
+where (product_id, change_date) In
+    (select product_id, max(change_date) as latest
+    from products
+    where change_date <='2019-08-16'
+    group by product_id))
+UNION
+(select product_id, 10 as price
+from products
+where product_id not in 
+            (select distinct product_id 
+            from Products 
+            where change_date <='2019-08-16'))
+order by product_id
 
 
